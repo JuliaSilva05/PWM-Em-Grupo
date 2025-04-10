3dc42e0cd0f28cfc9bdb1256567c9f64c80c678d
Rafael <rafaelssampaiobr@gmail.com>
2025-04-10 18:07:01 -0300

    resolvendo o erro e adicionando as keys

--- SiteComReact/src/config/parseConfig.js
+++ SiteComReact/src/config/parseConfig.js
@@ -2,9 +2,9 @@ import Parse from 'parse';
 
 // Inicializa o Parse com suas credenciais do Back4App
 Parse.initialize(
-  "SUA_APPLICATION_ID", // Substitua pela sua Application ID do Back4App
-  "SUA_CLIENT_KEY"     // Substitua pela sua Client Key do Back4App
-);
+  "SUA_wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+
 
 // Define o servidor do Back4App
 Parse.serverURL = 'https://parseapi.back4app.com/';
