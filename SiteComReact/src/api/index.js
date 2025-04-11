a3e4e8deb00b126e551f6ccfa9654817897992ea
G Martins <gabriel.00000844567@unicap.br>
2025-04-11 13:56:28 -0300

    adicionando as chaves e url do B4APP

---
+++ SiteComReact/src/api/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const urlPersonagem = "https://parseapi.back4app.com/classes/Personagem";
+const headers = {
+  "X-Parse-Application-Id": "vhhJsv1uKHWZCaQBf8iuwUhEBebIJSgWSmeSAE2e",
+  "X-Parse-REST-API-Key": "zQXCbWzj1QLsuOBVZsQey59ro4LnBAtJpX7jX40u",
+};
