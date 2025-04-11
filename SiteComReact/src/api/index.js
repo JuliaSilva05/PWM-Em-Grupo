e944f859069fd70bbbce6986a7c72d56018b4488
G Martins <gabriel.00000844567@unicap.br>
2025-04-11 14:09:17 -0300

    adicionando get e add personagem

--- SiteComReact/src/api/index.js
+++ SiteComReact/src/api/index.js
@@ -5,3 +5,27 @@ const headers = {
   "X-Parse-Application-Id": "vhhJsv1uKHWZCaQBf8iuwUhEBebIJSgWSmeSAE2e",
   "X-Parse-REST-API-Key": "zQXCbWzj1QLsuOBVZsQey59ro4LnBAtJpX7jX40u",
 };
+const headersJson = {
+  ...headers,
+  "Content-Type": "application/json",
+};
+
+export async function getPersonagens() {
+  try {
+    const response = await axios.get(urlPersonagem, { headers });
+    return response.data.results || [];
+  } catch (err) {
+    console.error("Erro ao buscar personagens:", err);
+    return [];
+  }
+}
+
+export async function addPersonagem(personagem) {
+  try {
+    const response = await axios.post(urlPersonagem, personagem, { headers: headersJson });
+    return { ...personagem, ...response.data };
+  } catch (err) {
+    console.error("Erro ao criar personagem:", err);
+    return null;
+  }
+}
